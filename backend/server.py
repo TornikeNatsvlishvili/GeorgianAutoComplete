bc0f20da4cef7309e7de1ef56bf5e6a31e0f7bb7
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-04-24 23:28:08 -0400

    add requirements

--- backend/server.py
+++ backend/server.py
@@ -4,19 +4,23 @@ import heapq
 app = Flask(__name__)
 
 word_suggestions = {}
+single_word_suggestions = []
 
 @app.route('/suggestion')
 def suggestion():
     if 'q' not in request.args:
         return 500
     query = request.args['q']
-    context = request.args['c']
+    context = request.args.get('c', '')
 
-    if context not in word_suggestions:
-        return jsonify({'suggestions': []})
+    if context == '':
+        suggestions = []
+        for count, word in single_word_suggestions:
+            if word.startswith(query):
+                suggestions.append(word)
     else:
-        count = len(word_suggestions[context])
-        sorted_suggestions = heapq.nlargest(count, word_suggestions[context])
+        count = len(word_suggestions['1.txt'][context])
+        sorted_suggestions = heapq.nlargest(count, word_suggestions['1.txt'][context])
         suggestions = []
         for count, word in sorted_suggestions:
             if word.startswith(query):
@@ -25,16 +29,26 @@ def suggestion():
     return jsonify({'suggestions': suggestions})
 
 
-if __name__ == '__main__':
-    # load autocomplete
-    with open('kb/1.txt', 'r') as f:
+def load_kb(name):
+    word_dict = {}
+    word_suggestions[name] = word_dict
+    with open('kb/{}'.format(name), 'r') as f:
         current_gram = ''
         for line in f:
             if not line.startswith('\t'):
                 current_gram = line.strip()
-                word_suggestions[current_gram] = []
+                word_dict[current_gram] = []
             else:
                 word, count = line.strip().split(':')
-                heapq.heappush(word_suggestions[current_gram], (int(count), word))
+                heapq.heappush(word_dict[current_gram], (int(count), word))
+
+if __name__ == '__main__':
+    # load autocomplete
+    load_kb("0.txt")
+    # for efficiency pre-sort and save all items since there is only one key
+    count = len(word_suggestions['0.txt'][''])
+    single_word_suggestions = heapq.nlargest(count, word_suggestions['0.txt'][''])
+
+    load_kb("1.txt")
 
     app.run(port=8080)
