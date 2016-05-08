1dd51a30632a790fdeef7886bc96b3ce951847df
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-05-07 22:26:10 -0700

    fix global variable

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 import heapq
+import logging
 
 server = Flask(__name__, static_url_path='')
 
+LOGGING_FILE_NAME = 'log.txt'
 word_suggestions = {}
 single_word_suggestions = []
 
 
 def load_kb(name):
     word_dict = {}
-    word_suggestions[name] = word_dict
     with open('kb/{}'.format(name), 'r') as f:
         current_gram = ''
         for line in f:
@@ -19,8 +20,14 @@ def load_kb(name):
             else:
                 word, count = line.strip().split(':')
                 heapq.heappush(word_dict[current_gram], (int(count), word))
+    word_suggestions[name] = word_dict
+
 
 def init():
+    global single_word_suggestions
+    logging.basicConfig(filename=LOGGING_FILE_NAME, level=logging.DEBUG)
+
+    print("initializing word store...")
     # load autocomplete
     load_kb("0.txt")
     # for efficiency pre-sort and save all items since there is only one key
@@ -28,6 +35,7 @@ def init():
     single_word_suggestions = heapq.nlargest(count, word_suggestions['0.txt'][''])
 
     load_kb("1.txt")
+    print("done")
 
 
 init()
@@ -59,5 +67,4 @@ def suggestion():
     return jsonify({'suggestions': suggestions})
 
 if __name__ == '__main__':
-
     server.run(host="0.0.0.0", port=8080)
--- backend/gunicorn.py
+++ backend/gunicorn.py
@@ -1,4 +1,3 @@
-import multiprocessing
-
 bind = 'unix:/tmp/gunicorn.sock'
-workers = multiprocessing.cpu_count() * 2 + 1
+# bind = '127.0.0.1:8080'
+workers = 1
