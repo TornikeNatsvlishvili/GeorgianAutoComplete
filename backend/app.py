18249b257e0b988e8a5f6736adfeb9c5439b26cb
tnatsvli <tnatsvli@mail.uoguelph.ca>
2016-05-24 00:49:33 -0700

    remove context aware temporarily, revert index.html

--- backend/app.py
+++ backend/app.py
@@ -31,18 +31,9 @@ def suggestion():
     query = request.args['q']
     context = request.args.get('c', '')
 
-    if context == '':
-        candidates = single_word_trie.get_all_data_with_prefix(query)
-        sorted_candidates = sorted(candidates, key = lambda word: word[1], reverse=True)[:NUM_SUGGESTIONS_TO_RETURN]
-        suggestions = [word for word, count in sorted_candidates]
-    else:
-        pass
-        # count = len(word_suggestions['1.txt'][context])
-        # sorted_suggestions = heapq.nlargest(count, word_suggestions['1.txt'][context])
-        # suggestions = []
-        # for count, word in sorted_suggestions:
-        #     if word.startswith(query):
-        #         suggestions.append(word)
+    candidates = single_word_trie.get_all_data_with_prefix(query)
+    sorted_candidates = sorted(candidates, key = lambda word: word[1], reverse=True)[:NUM_SUGGESTIONS_TO_RETURN]
+    suggestions = [word for word, count in sorted_candidates]
 
     return jsonify({'suggestions': suggestions[:NUM_SUGGESTIONS_TO_RETURN]})
 
